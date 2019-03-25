bead3e5e3f3a6288ff623fb2b0346884dd262692
ErkkaKorpi <33720000+ErkkaKorpi@users.noreply.github.com>
2019-03-25 17:16:18 +0200

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -9,6 +9,8 @@ SECRET_KEY=$4
 DB_USER=$5
 DB_PASS=$6
 
+docker swarm init
+
 echo $POSTGRES_PASSWORD | docker secret create db_secret -
 
 cd src && docker build --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD --build-arg BUCKET=$BUCKET --build-arg KEY_ID=$KEY_ID --build-arg SECRET_KEY=$SECRET_KEY --build-arg DB_USER=$DB_USER --build-arg DB_PASS=$DB_PASS -t photos-website .
